c6b9111db568eede8c3186c79ed671bc837e2784
Mateusz Korszun <mkorszun@gmail.com>
2013-01-15 10:08:37 +0100

    first commit

---
+++ src/main/java/com/cloudcontrolled/java/gwt/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package com.cloudcontrolled.java.gwt.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+  void greetServer(String input, AsyncCallback<String> callback)
+      throws IllegalArgumentException;
+}
